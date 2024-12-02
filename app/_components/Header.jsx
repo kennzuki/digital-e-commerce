4335ac3a8d8c338aa755bf0158bed2be2fcd90cd
kennzuki <kingsyken@gmail.com>
2024-12-02 22:51:17 +0300

    authenticatio refactor and debug

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,10 +1,13 @@
 import { Button } from '@/components/ui/button';
-import{ Store} from 'lucide-react';
+import { UserButton } from '@clerk/nextjs';
+import { Store } from 'lucide-react';
 
 const Header = () => {
   return (
     <nav className='bg-green-700 p-12 flex place-items-center justify-around shadow-xl shadow-black'>
-      <h1 className='underline font-extrabold text-2xl uppercase hover:scale-125'>Kenki store</h1>
+      <h1 className='underline font-extrabold text-2xl uppercase hover:scale-125'>
+        Kenki store
+      </h1>
       <ul className='flex gap-2 font-bold'>
         <li className=''>Home</li>
         <li className=''>Store</li>
@@ -15,6 +18,7 @@ const Header = () => {
         <Button className='bg-red-500 hover:scale-125 duration-75 ease-in-out shadow-xl shadow-black'>
           start selling
         </Button>
+        <UserButton className='h-[65px]' />
       </section>
     </nav>
   );
