cad1f383bfe49c5d1a9deeed51cb6b5a7776ee78
kennzuki <kingsyken@gmail.com>
2024-12-03 23:17:17 +0300

    products table schema

--- configs/schema.js
+++ configs/schema.js
@@ -1,8 +1,21 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { integer, pgTable, varchar } from 'drizzle-orm/pg-core';
 
-export const usersTable = pgTable("users", {
+export const usersTable = pgTable('users', {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).notNull(),
   image: varchar(),
   email: varchar({ length: 255 }).notNull().unique(),
+});
+
+export const productsTable = pgTable('products', {
+  id: integer().primaryKey(),
+  title: varchar().notNull(),
+  price: integer().notNull(),
+  description: varchar().notNull(),
+  about: varchar(),
+  category: varchar().notNull(),
+  imageUrl: varchar().notNull(),
+  fileUrl: varchar().notNull(),
+  message: varchar(),
+  createdBy: varchar('createdBy').notNull().references(()=>usersTable.email),
 });
