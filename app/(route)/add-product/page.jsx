5ec3b7f0091508f93e723fdbddb45fc724b28c63
kennzuki <kingsyken@gmail.com>
2024-12-03 22:48:45 +0300

    collecting form input values

--- app/(route)/add-product/page.jsx
+++ app/(route)/add-product/page.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+'use client'
+import React, { useState } from 'react';
 import { Input } from '@/components/ui/input';
 import {
   Select,
@@ -8,8 +9,8 @@ import {
   SelectValue,
 } from '@/components/ui/select';
 import { Textarea } from '@/components/ui/textarea';
-import { Button } from "@/components/ui/button";
-import ImageUpload from "./_components/ImageUpload";
+import { Button } from '@/components/ui/button';
+import ImageUpload from './_components/ImageUpload';
 
 const AddProduct = () => {
   const categoryOptions = [
@@ -23,39 +24,60 @@ const AddProduct = () => {
     'illustrations',
     'Others',
   ];
+  const [formData, setFormData] = useState([]);
+  const handleChange = (fieldName, fieldValue) => {
+      setFormData((prev) => ({ ...prev, [fieldName]: fieldValue }));
+      console.log(formData);
+      
+    };
+    const onAddProductBtn = () => { 
+        console.log(formData);
+        
+    }
+
   return (
     <div className='mt-12 px-32'>
       <h2 className='text-2xl font-bold'>Add New Product</h2>
       <p className=''>Start adding product details to sell your items</p>
-          <div className='grid grid-cols-1 md:grid-cols-2 gap-4 py-8'>
-              {/* left */}
-              <section className='flex flex-col gap-5'>
-                  <ImageUpload />
-                  <h4 className="">Upload file of product to sell</h4>
-                  <Input type="file" name='file' />
-                  
-                  <article className='mb-4'>
-                  <h4 className="font-bold">Message to user</h4>
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-4 py-8'>
+        {/* left */}
+        <section className='flex flex-col gap-5'>
+                  <ImageUpload onImageSelect={(e) => handleChange(e.target.name, e.target.files[0])} />
+                  <div>
+                  <h4 className=''>Upload file of product to sell</h4>
+                  <Input type='file' name='file'onChange={(e)=>handleChange(e.target.name,e.target.files[0])} />
+                  </div>
+          
+
+          <article className='mb-4'>
+            <h4 className='font-bold'>Message to user</h4>
             <Textarea
               name='description'
-              placeholder='write thank you msg to user'
+                          placeholder='write thank you msg to user'
+                          onChange={(e) => handleChange(e.target.name, e.target.value)}
             />
           </article>
-
-              </section>
-              {/* right */}
+        </section>
+        {/* right */}
         <section className='[&_h4]:font-medium [&_h4]:text-xl'>
           <article className='mb-4'>
             <h4 className=''>Product Title</h4>
-            <Input Placeholder='UI Kit in figma' name='title' />
+            <Input Placeholder='UI Kit in figma' name='title' onChange={(e) => handleChange(e.target.name, e.target.value)}/>
           </article>
           <article className='mb-4'>
             <h4 className=''>Product Title</h4>
-            <Input type='number' Placeholder='9.99' name='price' />
+            <Input
+              type='number'
+              Placeholder='9.99'
+              name='price'
+              onChange={(e) => handleChange(e.target.name, e.target.value)}
+            />
           </article>
-                  <article className='mb-4'>
-                      <h4 className="font-bold">Category</h4>
-            <Select>
+          <article className='mb-4'>
+            <h4 className='font-bold'>Category</h4>
+            <Select
+              onValueChange={(value) => handleChange('category', value)}
+            >
               <SelectTrigger className=''>
                 <SelectValue placeholder='Select Category' />
               </SelectTrigger>
@@ -72,18 +94,23 @@ const AddProduct = () => {
               </SelectContent>
             </Select>
           </article>
-                  <article className='mb-4'>
-                  <h4 className="font-bold">Description</h4>
+          <article className='mb-4'>
+            <h4 className='font-bold'>Description</h4>
             <Textarea
               name='description'
               placeholder='Add product description'
+              onChange={(e) => handleChange(e.target.name, e.target.value)}
             />
           </article>
-                  <article className='mb-4'>
-                  <h4 className="font-bold">About Product(Optional)</h4>
-            <Textarea name='about' placeholder='Add product information' />
-                  </article>
-                  <Button className='w-full'>Add Product</Button>
+          <article className='mb-4'>
+            <h4 className='font-bold'>About Product(Optional)</h4>
+            <Textarea
+              name='about'
+              placeholder='Add product information'
+              onChange={(e) => handleChange(e.target.name, e.target.value)}
+            />
+          </article>
+          <Button onClick={onAddProductBtn} className='w-full'>Add Product</Button>
         </section>
       </div>
     </div>
